976558b5650bb5595470ae3131fa34f409c20fe6
Collins Lagat <cklagat@gmail.com>
2024-01-12 14:48:07 +0300

    feat: add custom fields to account_move

--- models/account_move.py
+++ models/account_move.py
@@ -1,9 +1,15 @@
-from odoo import models
+from odoo import models, api, fields
+from odoo.tests.common import Form
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+    is_avior = fields.Boolean(related="fiscal_position_id.is_avior")
+    avior_amount = fields.Float(string="Avior", copy=False)
+    tax_on_shipping_address = fields.Boolean(
+        "Tax based on shipping address", default=True
+    )
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
