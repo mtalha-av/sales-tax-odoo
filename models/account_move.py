576747f40f809b917bd468eae05e8e662dfedce9
Collins Lagat <cklagat@gmail.com>
2024-01-12 14:52:02 +0300

    feat: when those fields in the line items and the avior_amount field changes, recalulate invoice figures

--- models/account_move.py
+++ models/account_move.py
@@ -11,5 +11,25 @@ class AccountMove(models.Model):
         "Tax based on shipping address", default=True
     )
 
+    @api.depends(
+        "line_ids.debit",
+        "line_ids.credit",
+        "line_ids.currency_id",
+        "line_ids.amount_currency",
+        "line_ids.amount_residual",
+        "line_ids.amount_residual_currency",
+        "line_ids.payment_id.state",
+        "avior_amount",
+    )
+    def _compute_amount(self):
+        res = super()._compute_amount()
+        for inv in self:
+            if not inv.avior_amount:
+                continue
+            inv.amount_tax = abs(inv.avior_amount)
+            inv.amount_total = inv.amount_untaxed + inv.amount_tax
+            sign = inv.move_type in ["in_refund", "out_refund"] and -1 or 1
+            inv.amount_total_signed = inv.amount_total * sign
+        return res
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
