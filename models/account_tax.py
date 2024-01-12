cd090bfb767f76974e8b9e96c474800e2cf5ffe9
Collins Lagat <cklagat@gmail.com>
2024-01-12 14:45:52 +0300

    feat: fetch tax managed by module from db that matches the tax rate or create it

--- models/account_tax.py
+++ models/account_tax.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import exceptions, fields, models, api, _
 
 
 class AccountTax(models.Model):
@@ -7,3 +7,40 @@ class AccountTax(models.Model):
     _inherit = "account.tax"
 
     is_avior = fields.Boolean()
+
+    @api.model
+    def get_avior_tax(self, tax_rate):
+        """
+        Returns the Avior Tax record for the given tax rate.
+        If the record does not exist, it creates it from the Avior Tax template.
+        """
+        domain = [
+            ("amount", "=", tax_rate),
+            ("is_avior", "=", True),
+            ("company_id", "=", self.env.company.id),
+        ]
+        tax = self.with_context(active_test=False).search(domain, limit=1)
+        if tax and not tax.active:
+            tax.active = True
+        if not tax:
+            domain = [
+                ("amount", "=", 0),
+                ("is_avior", "=", True),
+                ("company_id", "=", self.env.company.id),
+            ]
+            tax_template = self.search(domain, limit=1)
+            if not tax_template:
+                raise exceptions.UserError(
+                    _("Please configure Avior Tax for Company %s:")
+                    % self.env.company.name
+                )
+            # If you get a unique constraint error here,
+            # check the data for your existing Avior taxes.
+            vals = {
+                "amount": tax_rate,
+                "name": _("{}%*").format(str(tax_rate)),
+            }
+            tax = tax_template.sudo().copy(default=vals)
+            # Odoo core does not use the name set in default dict
+            tax.name = vals.get("name")
+        return tax
